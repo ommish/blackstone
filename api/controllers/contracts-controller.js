9ea90c3b6aea61d6ff540eefa8a9392fe4fcb3c4
Ommi Shimizu <oshimizu15@gmail.com>
2019-09-03 08:22:07 -0400

    Add capability to model timer events and validate duration values
    Signed-off-by: Ommi Shimizu <oshimizu15@gmail.com>

--- api/controllers/contracts-controller.js
+++ api/controllers/contracts-controller.js
@@ -1119,6 +1119,94 @@ const createTransitionCondition = (processAddress, dataType, gatewayId, activity
   });
 });
 
+const addBoundaryEvent = (pdAddress, {
+  attachedTo, id, eventType, eventBehavior, conditionalValue = {}, fixedValue = 0,
+}) => new Promise((resolve, reject) => {
+  log.info('REQUEST: Add boundary event with data: %s', JSON.stringify({
+    pdAddress,
+    attachedTo,
+    id,
+    eventType,
+    eventBehavior,
+    conditionalValue,
+    fixedValue,
+  }));
+  const processDefinition = getContract(global.__abi, global.__bundles.BPM_MODEL.contracts.PROCESS_DEFINITION, pdAddress);
+  processDefinition.addBoundaryEvent(
+    global.stringToHex(attachedTo),
+    global.stringToHex(id),
+    eventType,
+    eventBehavior,
+    global.stringToHex(conditionalValue.dataPath || ''),
+    global.stringToHex(conditionalValue.dataStorageId || ''),
+    conditionalValue.dataStorage || '',
+    fixedValue,
+    (error) => {
+      if (error) return reject(boomify(error, `Failed to add boundary event ${id} to process definition ${pdAddress}`));
+      log.info(`SUCCESS: Added boundary event ${id} to process definition ${pdAddress}`);
+      return resolve();
+    },
+  );
+});
+
+const addBoundaryEventAction = (pdAddress, {
+  id, escalationAction: {
+    dataPath, dataStorageId, dataStorage, fixedTarget, actionFunction,
+  },
+}) => new Promise((resolve, reject) => {
+  log.debug('REQUEST: Add boundary event action with data: %s', JSON.stringify({
+    pdAddress,
+    id,
+    dataPath,
+    dataStorageId,
+    dataStorage,
+    fixedTarget,
+    actionFunction,
+  }));
+  const processDefinition = getContract(global.__abi, global.__bundles.BPM_MODEL.contracts.PROCESS_DEFINITION, pdAddress);
+  processDefinition.addBoundaryEventAction(
+    global.stringToHex(id),
+    global.stringToHex(dataPath),
+    global.stringToHex(dataStorageId),
+    dataStorage || '',
+    fixedTarget || '',
+    actionFunction,
+    (error) => {
+      if (error) return reject(boomify(error, `Failed to add boundary event action for event ${id} to process definition ${pdAddress}`));
+      log.info(`SUCCESS: Added boundary event action for event ${id} to process definition ${pdAddress}`);
+      return resolve();
+    },
+  );
+});
+
+const createIntermediateEvent = (pdAddress, {
+  id, eventType, eventBehavior, conditionalValue = {}, fixedValue = 0,
+}) => new Promise((resolve, reject) => {
+  log.debug('REQUEST: Add intermediate catch event with data: %s', JSON.stringify({
+    pdAddress,
+    id,
+    eventType,
+    eventBehavior,
+    conditionalValue,
+    fixedValue,
+  }));
+  const processDefinition = getContract(global.__abi, global.__bundles.BPM_MODEL.contracts.PROCESS_DEFINITION, pdAddress);
+  processDefinition.createIntermediateEvent(
+    global.stringToHex(id),
+    eventType,
+    eventBehavior,
+    global.stringToHex(conditionalValue.dataPath || ''),
+    global.stringToHex(conditionalValue.dataStorageId || ''),
+    conditionalValue.dataStorage || '',
+    fixedValue,
+    (error) => {
+      if (error) return reject(boomify(error, `Failed to add intermediate catch event ${id} to process definition ${pdAddress}`));
+      log.info(`SUCCESS: Added intermediate catch event ${id} to process definition ${pdAddress}`);
+      return resolve();
+    },
+  );
+});
+
 const signAgreement = (actingUserAddress, agreementAddress) => new Promise(async (resolve, reject) => {
   log.debug('REQUEST: Sign agreement %s by user %s', agreementAddress, actingUserAddress);
   try {
@@ -1486,6 +1574,9 @@ module.exports = {
   createTransition,
   setDefaultTransition,
   createTransitionCondition,
+  addBoundaryEvent,
+  addBoundaryEventAction,
+  createIntermediateEvent,
   completeActivity,
   signAgreement,
   getModelAddressFromId,
