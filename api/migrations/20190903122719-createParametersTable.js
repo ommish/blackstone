39863aa2129d8df5b8b14974123361d593fd1110
Ommi Shimizu <oshimizu15@gmail.com>
2019-09-03 09:36:14 -0400

    Fix parameters table migration
    Signed-off-by: Ommi Shimizu <oshimizu15@gmail.com>

--- api/migrations/20190903122719-createParametersTable.js
+++ api/migrations/20190903122719-createParametersTable.js
@@ -18,22 +18,21 @@ exports.up = async (db) => {
     data_type int NOT NULL,
     label VARCHAR(255) NOT NULL
   );`);
-  await db.runSql(`INSERT INTO parameter_types(id, data_type, label) VALUES(
-    (0, 1, "Boolean"),
-    (1, 2, "Text (short)"),
-    (2, 18, "Number"),
-    (3, 8, "Date"),
-    (4, 8, "Datetime"),
-    (5, 18, "Monetary Amount"),
-    (6, 40, "User/Organization"),
-    (7, 40, "Contract Address"),
-    (8, 40, "Signing Party"),
-    (9, 59, "Bytes32"),
-    (10, 2, "Document"),
-    (11, 2, "Text (large)"),
-    (12, 8, "Number (positive)"),
-    (13, 2, "Duration"),
-  )`);
+  await db.runSql(`INSERT INTO parameter_types(id, data_type, label) VALUES
+    (0, 1, 'Boolean'),
+    (1, 2, 'Text (short)'),
+    (2, 18, 'Number'),
+    (3, 8, 'Date'),
+    (4, 8, 'Datetime'),
+    (5, 18, 'Monetary Amount'),
+    (6, 40, 'User/Organization'),
+    (7, 40, 'Contract Address'),
+    (8, 40, 'Signing Party'),
+    (9, 59, 'Bytes32'),
+    (10, 2, 'Document'),
+    (11, 2, 'Text (large)'),
+    (12, 8, 'Number (positive)'),
+    (13, 2, 'Duration');`);
 };
 
 exports.down = async (db) => {
